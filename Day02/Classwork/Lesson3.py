8f4f7603728130168d1ceffc77418fe0d8de71da
Shagu-blip <datkavarshagula@gmail.com>
2024-11-12 18:49:58 +0400

    1

---
+++ Day02/Classwork/Lesson3.py
@@ -0,0 +1,51 @@
+#მონაცემთა ტიპები
+#str(string)-int(integer)-float()
+
+#str-სიტყვა,წსინადადება.
+#print("hello")
+
+#int-მთელი რიცხვი.
+#1,2,3,4,5,6,7,...
+
+#float-1.2,6.3,7.1,...
+
+#print(type("gamarjoba"))
+#print(type(6))
+#print(type(4.8))
+
+#-----------
+
+#ცვლადების/variables
+#რისგან შედგება ცვლადი-სახელი-მნიშვნელობა-(name;value)
+#name="daviti"
+
+#country="georgia"
+
+#academy="goa"
+
+#group=52
+
+#print(name)=print("name")
+
+#print(group-2)=50
+
+#----------------
+
+#მათემატიკური ოპერაციები
+
+#+;-;/;*
+
+#print(1+1)
+#print(2-1)
+#print(20/5)
+#print(1200*7)
+
+#daviti=34
+#name=daviti
+#print(name)
+
+#print('daviti')=print("daviti")-სტანდარტი
+
+age=2
+age=20
+print(age)
