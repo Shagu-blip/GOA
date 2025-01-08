df76c95100e999ac6900476a79359140f986363f
Shagu-blip <datkavarshagula@gmail.com>
2025-01-08 20:01:09 +0400

    Add files via upload

---
+++ Day08/Classwork/lesson9.py
@@ -0,0 +1,12 @@
+number1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+number2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+
+plus=number1+number2
+divide=number1/number2
+multiplication=number1*number2
+minus=number1-number2
+
+print(plus)
+print(divide)
+print(multiplication)
+print(minus)
