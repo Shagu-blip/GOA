a577cd8ac76b15764d4cda1bad8d2a355f8afb5a
Shagu-blip <datkavarshagula@gmail.com>
2025-01-08 20:02:00 +0400

    Add files via upload

---
+++ Day10/Classwork/level11.py
@@ -0,0 +1,65 @@
+#N1
+print(10 > 5)      
+print(7 > 3)       
+print(-2 > -5)      
+print(0 > -1)       
+print(50 > 40)      
+print(3 > 3)        
+print(-10 > -9)     
+print(100 > 200)   
+print(194 > 104)    
+print(295 > 93)     
+
+#N2
+print(10 > 5)       
+print(7 > 3)        
+print(-2 > -5)      
+print(0 > -1)       
+print(50 > 40)     
+print(3 > 3)        
+print(-10 > -9)     
+print(100 > 200)    
+print("b" > "a")    
+print("kata" > "dzagli")
+
+#N3
+print(5 != 10)    
+print(10 != 10)    
+print(-2 != 2)      
+print(0 != 1)       
+print(50 != 50)     
+print(3.14 != 2.71) 
+print("a" != "b")   
+print("kata " == "dzagli")
+print("lamborghini" != "Porche") 
+print(5 != 5)       
+
+#N4
+print(10 == 10)     
+print(5 == 10)     
+print(-2 == -2)     
+print(0 == 0)       
+print(50 == 50)     
+print(3.14 == 2.71) 
+print("a" == "a")   
+print("kata" == "kata")
+print(-5.01 == 5.01) 
+print(5 == 5)
+
+#N5
+stringi1= "hello"
+stringi2= "hello"
+
+print(stringi1 == stringi2)
+print(stringi1 != stringi2) 
+print(stringi1 > stringi2)  
+print(stringi1 < stringi2)   
+
+#N6
+stringi1 = "ananasi"
+stringi2 = "mango"
+
+print(stringi1 == stringi2)  
+print(stringi1 != stringi2) 
+print(stringi1 > stringi2)  
+print(stringi1 < stringi2)  
